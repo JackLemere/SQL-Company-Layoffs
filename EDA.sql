df065bb3a117edc6a65f56070e1369391b86bf51
Jack Lemere <72566999+JackLemere@users.noreply.github.com>
2024-05-28 18:58:43 -0400

    Add files via upload

--- EDA.sql
+++ EDA.sql
@@ -2,50 +2,52 @@
 
 SELECT * FROM layoffs_staging2;
 
+-- Highest Total Laid Off
 SELECT MAX(total_laid_off) FROM layoffs_staging2;
 
+-- Companies who laid off all employees
 SELECT * FROM layoffs_staging2
 WHERE percentage_laid_off = 1
 ORDER BY total_laid_off DESC;
 
+-- Companies and their total laid off
 SELECT company, SUM(total_laid_off) FROM layoffs_staging2
 GROUP BY company
 ORDER BY 2 DESC;
 
+-- Identify Date Range
 SELECT MIN(`date`), MAX(`date`) FROM layoffs_staging2;
 
+
+-- Locations ordered by most laid off
 SELECT location, SUM(total_laid_off) FROM layoffs_staging2
 GROUP BY location
 ORDER BY 2 DESC;
 
 SELECT * FROM layoffs_staging2;
 
+-- Countries ordered by most layoffs
 SELECT country, SUM(total_laid_off) FROM layoffs_staging2
 GROUP BY country
 ORDER BY 2 DESC;
 
-SELECT * FROM layoffs_staging2
-WHERE company LIKE "Deliveroo%";
-
-SELECT country, SUM(total_laid_off) FROM layoffs_staging2
-GROUP BY country
-ORDER BY 2 DESC;
-
+-- Years ordered by most layoffs
 SELECT YEAR(`date`), SUM(total_laid_off) FROM layoffs_staging2
 GROUP BY 1
 ORDER BY 1 DESC;
 
+-- Company stages ordered by most layoffs
 SELECT stage, SUM(total_laid_off) FROM layoffs_staging2
 GROUP BY stage
 ORDER BY 2 DESC;
 
-
+-- Months ordered by most layoffs
 SELECT SUBSTRING(`date`, 1, 7) AS `MONTH`, SUM(total_laid_off) FROM layoffs_staging2
 WHERE SUBSTRING(`date`, 1, 7) IS NOT NULL
 GROUP BY `MONTH`
 ORDER BY 1 ASC;
 
--- Rolling SUM
+-- Rolling total of layoffs over time
 
 WITH rolling_total AS
 (
@@ -57,22 +59,7 @@ ORDER BY 1 ASC
 SELECT `MONTH`, total_off, SUM(total_off) OVER(ORDER BY `MONTH`) AS roll_total
 FROM rolling_total;
 
-
-WITH tempquery AS
-(
-SELECT company, SUBSTRING(`date`,1, 4) AS `Date`, SUM(total_laid_off) AS sum FROM layoffs_staging2
-GROUP BY company, `Date`
-ORDER BY company ASC
-)
-SELECT MAX(sum) FROM tempquery;
-
-WITH tempquery AS
-(
-SELECT company, SUBSTRING(`date`,1, 4) AS `Date`, SUM(total_laid_off) AS sum FROM layoffs_staging2
-GROUP BY company, `Date`
-ORDER BY company ASC
-)
-SELECT *, MAX(sum) OVER () AS MAX FROM tempquery;
+-- Top 5 companies with most layoffs by year
 
 WITH Company_Year  (company, years, total_laid_off) AS
 (
@@ -86,34 +73,8 @@ SELECT * FROM Company_Year_Rank
 WHERE ranking <= 5
 ORDER BY years;
 
--- IDEAS TO CONTINUE
 
-
--- Try to find top 5 companies with most layoffs by month, and determine which companies occur in the top 5 the most
-
-SELECT SUBSTRING(`date`,1,7), company, total_laid_off FROM layoffs_staging2
-ORDER BY 1;
-
-SELECT `date`, company, total_laid_off FROM layoffs_staging2
-ORDER BY 1;
-
-WITH MonthlyLaidOff AS
-(
-SELECT SUBSTRING(`date`,1,7) AS YearMonth, company, industry, total_laid_off FROM layoffs_staging2
-ORDER BY YearMonth
-), MonthlyRanking AS
-(
-SELECT *, DENSE_RANK() OVER (PARTITION BY YearMonth ORDER BY total_laid_off DESC) AS Ranking FROM MonthlyLaidOff
-)
-SELECT * FROM MonthlyRanking
-WHERE YearMonth IS NOT NULL AND Ranking <= 5;
-
--- Companies sometimes have layoffs multiple times in the same month (Uber 2020-05 for example), let's try to merge these into a total for the month
-
-
-SELECT SUBSTRING(`date`,1,7) AS YearMonth, company, SUM(total_laid_off) FROM layoffs_staging2
-GROUP BY YearMonth, company
-ORDER BY YearMonth;
+-- Top 5 companies with most layoffs by month
 
 WITH MonthlyLaidOff AS
 (
@@ -127,7 +88,10 @@ SELECT * FROM MonthlyRanking
 WHERE YearMonth IS NOT NULL AND Ranking <= 5 AND sum_laid_off IS NOT NULL
 ORDER BY YearMonth;
 
--- We now have a top 5 ranking of total layoffs in each month, time to find out which companies appear in the top 5 the most
+
+-- Companies who appear most in the top 5 layoffs per month
+
+-- Using a Temp Table
 
 CREATE TEMPORARY TABLE monthly_rankings
 WITH MonthlyLaidOff AS
@@ -145,3 +109,25 @@ ORDER BY YearMonth;
 SELECT COUNT(company) AS Times_in_Top_5_layoffs, company, industry FROM monthly_rankings
 GROUP BY company, industry
 ORDER BY 1 DESC;
+
+-- Alternatively we can use CTEs to achieve the same output
+
+WITH MonthlyLaidOff AS
+(
+SELECT SUBSTRING(`date`,1,7) AS YearMonth, company, industry, SUM(total_laid_off) AS sum_laid_off FROM layoffs_staging2
+GROUP BY YearMonth, company, industry
+), MonthlyRanking AS
+(
+SELECT *, DENSE_RANK() OVER (PARTITION BY YearMonth ORDER BY sum_laid_off DESC) AS Ranking FROM MonthlyLaidOff
+), TopFiveRanking AS
+(
+SELECT * FROM MonthlyRanking
+WHERE YearMonth IS NOT NULL AND Ranking <= 5 AND sum_laid_off IS NOT NULL
+ORDER BY YearMonth
+), TopFiveCount AS
+(
+SELECT COUNT(company) AS Times_in_Top_5_layoffs, company, industry FROM TopFiveRanking
+GROUP BY company, industry
+ORDER BY 1 DESC
+)
+SELECT * FROM TopFiveCount;
